a352b068994525315e339efc08e56eace7cce6ed
ugoMusk <ugobothankgod@gmail.com>
2023-08-01 19:45:04 +0100

    implement a binary tree without child nodes

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_t *binary_tree_node - a binary tree with empty child nodes
+ * binary_tree_t *parent - pointer to parent node
+ * value - holds data type of a given node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+  
+}
