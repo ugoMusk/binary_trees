7695c906d822812fa6c60ca0fc444947f89ccf3c
SAMKE-glitch <sammythemwa@gmail.com>
2023-08-02 02:51:05 -0700

    deleted the tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - function that deletes the whole binary tree
+ * @tree: pointer to the tree to be deleted
+ * Return: void
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
