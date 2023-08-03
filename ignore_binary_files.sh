45b3f9061971d89952468f90f1a20c0d926ac726
Thankgod Ugobo(ugoMuski) <34632702+ugoMusk@users.noreply.github.com>
2023-08-03 22:00:40 +0100

    Delete ignore_binary_files.sh

--- ignore_binary_files.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-file=".gitignore";
-main_files="$(ls | grep main.c)";
-binary_files=$(find . \! -regex '.*/.*/.*' -executable -type f | sed -e 's%^\./%%');
-
-for bin in $binary_files;do grep -xqF -- "$bin" "$file" || echo "$bin"  >> "$file";done
-	   
-for mfile in $main_files;do grep -xqF -- "$mfile" "$file" || echo "$mfile" >> "$file";done
