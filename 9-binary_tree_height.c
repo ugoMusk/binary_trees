b2c5e48f40bbd6ce11a739ad49feab2313d8b5d9
ugoMusk <ugobothankgod@gmail.com>
2023-08-04 06:53:51 +0100

    fix error in code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the heigh of a tree
- * @tree: a pointer to the root node of the tree to measure height
+ * binary_tree_height - measures the height of a tree
+ * @tree: a pointer to the root node of the tree
  * Return: if tree is NULL, return 0 otherise height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		size_t l = 0, r = 0;
 
 		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		r = tree->right ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((l > r) ? l : r);
 	}
 	return (0);
